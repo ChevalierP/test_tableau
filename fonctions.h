f60ad975c0028e541fddacf1c68d4a2c1abb00c6
ChevalierP <chevalierp01@gmail.com>
2013-07-20 16:27:12 +0200

    tableaux

---
+++ fonctions.h
@@ -0,0 +1,6 @@
+#ifndef FONCTIONS_H_INCLUDED
+#define FONCTIONS_H_INCLUDED
+
+int sommeTableau(int tableau[], int tailleTableau);
+
+#endif // FONCTIONS_H_INCLUDED
---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "fonctions.h"
+
+int main()
+{
+    int tableau[4];
+    printf("Case 1 : ");
+    scanf("%d",&tableau[0]);/*remplissage du tableau par l'utilisateur*/
+    printf("\nCase 2 : ");
+    scanf("%d",&tableau[1]);
+    printf("\nCase 3 : ");
+    scanf("%d",&tableau[2]);
+    printf("\nCase 4 : ");
+    scanf("%d",&tableau[3]);
+    printf("\n La somme de ces valeurs est : %d",sommeTableau(tableau,4));
+    return 0;
+}
+int sommeTableau(int tableau[], int tailleTableau)
+{
+    int temporaire=0, i=0;
+    for(i=0; i<tailleTableau;i++)
+    {
+        temporaire += tableau[i];
+    }
+return temporaire;
+}
